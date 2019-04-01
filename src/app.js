8ccefdffcc56db97cc92c7b2c3f7ec3fb4e5728d
rahul-kumar4 <Rahul.kumar4@digiscloud.com>
2019-04-01 17:56:53 +0530

    Require changes

--- src/app.js
+++ src/app.js
@@ -5,17 +5,13 @@ const express = require('express');
 
 const app = express();
 
+const { accounts, users, writeJSON } = require('./data');
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
-
-const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
-const users = JSON.parse(userData);
 
 app.get('/', (req, res) => {
     res.render('index', { title: 'Account Summary', accounts });
@@ -40,8 +36,7 @@ app.post('/transfer', (req, res) => {
      accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
      accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) +
      parseInt(req.body.amount, 10);
-     const accountsJSON = JSON.stringify(accounts, null, 4);
-     fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+     writeJSON();
      res.render('transfer', { message : "Transfer Completed"});
 });
 app.get('/payment', (req, res) => {
@@ -50,9 +45,8 @@ app.get('/payment', (req, res) => {
 app.post('/payment', (req, res) => {
    accounts.credit.balance -= req.body.amount;
    accounts.credit.available += parseInt(req.body.amount, 10);
-   accountsJSON = JSON.stringify(accounts, null, 4);
-   fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
-     res.render('payment', { message : "Payment Successful", account: accounts.credit });
+   writeJSON();
+   res.render('payment', { message : "Payment Successful", account: accounts.credit });
 });
 
 app.listen(3000, () => console.log('Project is running on port 3000'));
